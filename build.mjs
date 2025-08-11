6a2f2b968d8afd6619efe078d67ecae98922b454
Mouhamadou Moustapha Diagne <mousdef34@gmail.com>
2025-08-11 13:50:30 +0000

    fix: fixed some errors

--- build.mjs
+++ build.mjs
@@ -3,6 +3,7 @@ import { build } from 'esbuild';
 await build({
   entryPoints: ['src/Modal.tsx'],
   outfile: 'dist/index.js',
+  external: ['react', 'react-dom'], 
   bundle: true,
   format: 'esm',
   target: ['es2020'],
