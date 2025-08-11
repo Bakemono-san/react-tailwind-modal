f7f03ccefab3724b6b859957e2fc0b89189d7c97
Mouhamadou Moustapha Diagne <mousdef34@gmail.com>
2025-08-11 12:27:36 +0000

    feat: Removed the node_modules

--- dist/Modal.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-type ModalProps = {
-    isOpen: boolean;
-    onClose: () => void;
-    children: React.ReactNode;
-};
-export declare const Modal: React.FC<ModalProps>;
-export {};
--- dist/Modal.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-export const Modal = ({ isOpen, onClose, children }) => {
-    if (!isOpen)
-        return null;
-    return (_jsx("div", { className: "fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50", children: _jsxs("div", { className: "bg-white rounded-lg shadow-lg w-full max-w-md p-6 relative", children: [_jsx("button", { onClick: onClose, className: "absolute top-2 right-2 text-gray-500 hover:text-gray-800", children: "\u2715" }), children] }) }));
-};
